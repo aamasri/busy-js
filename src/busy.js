cc8e2bec3b0a8a818974d485b23607fc15c1fe25
ananda <ananda@auroraweb.ca>
2023-11-07 19:24:52 -0700

    Release version 1.2.0 - More robust queuing and better modal effect

--- src/busy.js
+++ src/busy.js
@@ -1,7 +1,5 @@
 /**
  * @fileOverview busy animation
- * @author Ananda Masri
- * @version 1.0.0
  */
 
 const debug = false;
@@ -10,41 +8,33 @@ import './busy.styl';
 
 let busyAnimation = false;
 let modalOverlay = false;
-export const queue = [];
+let fadeoutAnimationTimer = false;
+let fadeoutModalTimer = false;
+export let queue = [];
 
 
 
 // start loading animation
 export async function start(id, timeout=7, modal=false) {
+    window.clearTimeout(fadeoutAnimationTimer);
     id = id ? id.replace(/'/g, '') : '';	// strip any single quotes from id
     timeout = (typeof timeout === 'number') ? timeout * 1000 : 7000;
-    const expiry = Date.now() + timeout;
 
-    queue.push({ id: id, expiry: expiry, modal: modal });     // allows matching id to be stopped
-
-    if (!busyAnimation) {
-        const isInIframe = window.location !== window.parent.location;		// detects whether we're in an iframe
-        const body = isInIframe ? parent.document.body : document.body;
-
-        modalOverlay = document.createElement('div');
-        modalOverlay.id = 'animated-loader-modal-overlay';
-        document.body.appendChild(modalOverlay);
-
-        busyAnimation = document.createElement('div');
-        busyAnimation.id = 'animated-loader';
-        busyAnimation.title = id;
-        busyAnimation.innerHTML = '<div><div><div><div><div><div><div><div><div><div></div></div></div></div></div></div></div></div></div>';
-        body.appendChild(busyAnimation);
+    const duplicateItem = queue.findIndex(item => item.id === id);
+    if (duplicateItem > -1) {
+        queue.splice(duplicateItem, 1);
+        if (debug) console.debug(`busy item "${id}" already in queue!`);
     }
 
-    busyAnimation.style.zIndex = onTopZIndex().toString();
-    busyAnimation.style.display = 'block';   // fade in
-    busyAnimation.style.opacity = '1';   // fade in
+    // the queue is the master list controlling the busy animation
+    queue.push({
+        id: id,
+        expiry: Date.now() + timeout,
+        modal: modal
+    });
 
-    if (modal)
-        modalOverlay.style.opacity = '0.4';   // fade in
-
-    window.setTimeout(() => { stop(id); }, timeout);   // dequeue
+    _updateBusy();
+    window.setTimeout(() => { stop(id); }, timeout);   // dequeue item after timeout
 }
 
 
@@ -52,51 +42,41 @@ export async function start(id, timeout=7, modal=false) {
 
 // stop loading animation
 export function stop(id) {
-    if (!queue.length)
-        return;
+    if (queue.length) {
+        id = id ? id.toString().replace(/'/g, '') : '';	// strip any single quotes from id
+        let arrayIndex;
 
-    id = id ? id.toString().replace(/'/g, '') : '';	// strip any single quotes from id
-    let arrayIndex;
-
-    // pop specified busy id off queue
-    if (id) {
-        arrayIndex = queue.findIndex(busyItem => busyItem.id === id);
-        if (arrayIndex > -1) {
-            if (debug) console.debug(`busy item "${queue[arrayIndex].id}" completed before timeout!`);
-            queue.splice(arrayIndex, 1);
+        // pop specified busy id off queue
+        if (id) {
+            arrayIndex = queue.findIndex(busyItem => busyItem.id === id);
+            if (arrayIndex > -1) {
+                if (debug) console.debug(`busy item "${queue[arrayIndex].id}" stopped!`);
+                queue.splice(arrayIndex, 1);
+            }
         }
+
+        // remove any expired item from the queue
+        queue.forEach(item => {
+            if (item.expiry <= Date.now()) {
+                arrayIndex = queue.findIndex(busyItem => busyItem.id === item.id);
+                if (arrayIndex > -1) {
+                    if (debug) console.debug(`removing expired busy item "${queue[arrayIndex].id}"`);
+                    queue.splice(arrayIndex, 1);
+                }
+            }
+        })
     }
 
-    // find any expired busy item
-    arrayIndex = queue.findIndex(busyItem => busyItem.expiry <= Date.now());
-    if (arrayIndex > -1) {
-        console.warn(`busy indication timed out waiting for "${queue[arrayIndex].id}" to finish!`);
-        queue.splice(arrayIndex, 1);
-    }
-
-    // kill modal overlay if no modal items left in queue
-    arrayIndex = queue.findIndex(busyItem => busyItem.modal);
-    if (arrayIndex === -1)
-        _hideModalOverlay();
-
-    if (queue.length === 0) {
-        _hideModalOverlay();
-        busyAnimation.style.opacity = '0';		// fade out
-    }
+    _updateBusy();
 }
 
-
-
-function _hideModalOverlay() {
-    modalOverlay.style.opacity = '0';   // fade out
-}
 
 
 
 // clear the busy queue and stop the busy animation
 export function reset() {
-    queue.length = 0;
-    busyAnimation.style.opacity = '0';		// fade out
+    queue = [];
+    _updateBusy();
 }
 
 
@@ -105,6 +85,83 @@ export function reset() {
 export function status() {
     console.log('busy queue:', queue);
 }
+
+
+
+// monitor the queue and update the busy animation
+function _updateBusy() {
+    if (debug) console.debug(`_updateBusy() queue length: ${queue.length}`);
+    if (queue.length === 0) {
+        _fadeOutAnimation();
+        _fadeOutModal();
+    } else {
+        _showAnimation(queue[0].id);
+        if (queue.some(item => item.modal))
+            _showModal()
+        else
+            _fadeOutModal();
+    }
+}
+
+
+
+function _showAnimation(id) {
+    window.clearTimeout(fadeoutAnimationTimer);
+
+    // create busy animation elements if it doesn't exist (they can be hidden but never removed)
+    if (!busyAnimation) {
+        // detects whether we're in an iframe - we always want to attach the busy animation to the root document
+        const isInIframe = window.location !== window.parent.location;
+        const body = isInIframe ? parent.document.body : document.body;
+
+        modalOverlay = document.createElement('div');
+        modalOverlay.id = 'animated-loader-modal-overlay';
+        document.body.appendChild(modalOverlay);
+
+        busyAnimation = document.createElement('div');
+        busyAnimation.id = 'animated-loader';
+        busyAnimation.innerHTML = '<div><div><div><div><div><div><div><div><div><div></div></div></div></div></div></div></div></div></div>';
+        body.appendChild(busyAnimation);
+    }
+
+    const topZ = onTopZIndex().toString();
+    busyAnimation.style.zIndex = topZ;
+    modalOverlay.style.zIndex = topZ;
+    busyAnimation.title = id;
+    busyAnimation.style.display = 'block';
+    busyAnimation.style.opacity = '1';
+}
+
+function _showModal() {
+    window.clearTimeout(fadeoutModalTimer);
+    modalOverlay.style.display = 'block';
+    modalOverlay.style.opacity = '1';
+}
+
+function _fadeOutAnimation() {
+    window.clearTimeout(fadeoutAnimationTimer);
+
+    // fade out
+    busyAnimation.style.opacity = '0';
+
+    // then hide
+    fadeoutAnimationTimer = window.setTimeout(() => {
+        busyAnimation.style.display = 'none';
+    }, 500);   // hide
+}
+
+function _fadeOutModal() {
+    window.clearTimeout(fadeoutModalTimer);
+
+    // fadeout
+    modalOverlay.style.opacity = '0';
+
+    // then hide
+    fadeoutModalTimer = window.setTimeout(() => {
+        modalOverlay.style.display = 'none';
+    }, 500);   // hide
+}
+
 
 
 
