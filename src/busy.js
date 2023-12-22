2b3813e48388fcca386a3e5652feb7334a66c2a5
ananda <ananda@auroraweb.ca>
2023-12-21 19:53:14 -0700

    Release version 1.2.5 - Scoped busy back to it's local document (even when its in an iframe).

--- src/busy.js
+++ src/busy.js
@@ -15,27 +15,27 @@ let fadeoutModalTimer;
 
 // start loading animation
 export function start(id, timeout=7, modal=false) {
-    window.top.clearTimeout(fadeoutAnimationTimer);
+    window.clearTimeout(fadeoutAnimationTimer);
     id = id ? id.replace(/'/g, '') : '';	// strip any single quotes from id
     timeout = (typeof timeout === 'number') ? timeout * 1000 : 7000;
 
     _initQueue();
 
-    const duplicateItem = window.top.busyQueue.findIndex(item => item.id === id);
+    const duplicateItem = window.busyQueue.findIndex(item => item.id === id);
     if (duplicateItem > -1) {
-        window.top.busyQueue.splice(duplicateItem, 1);
+        window.busyQueue.splice(duplicateItem, 1);
         if (debug) console.debug(`busy item "${id}" already in queue!`);
     }
 
     // the queue is the master list controlling the busy animation
-    window.top.busyQueue.push({
+    window.busyQueue.push({
         id: id,
         expiry: Date.now() + timeout,
         modal: modal
     });
 
     _updateBusy();
-    window.top.setTimeout(() => { stop(id); }, timeout);   // dequeue item after timeout
+    window.setTimeout(() => { stop(id); }, timeout);   // dequeue item after timeout
 }
 
 
@@ -45,26 +45,26 @@ export function start(id, timeout=7, modal=false) {
 export function stop(id) {
     _initQueue();
 
-    if (window.top.busyQueue.length) {
+    if (window.busyQueue.length) {
         id = id ? id.toString().replace(/'/g, '') : '';	// strip any single quotes from id
         let arrayIndex;
 
         // pop specified busy id off queue
         if (id) {
-            arrayIndex = window.top.busyQueue.findIndex(busyItem => busyItem.id === id);
+            arrayIndex = window.busyQueue.findIndex(busyItem => busyItem.id === id);
             if (arrayIndex > -1) {
-                if (debug) console.debug(`busy item "${window.top.busyQueue[arrayIndex].id}" stopped!`);
-                window.top.busyQueue.splice(arrayIndex, 1);
+                if (debug) console.debug(`busy item "${window.busyQueue[arrayIndex].id}" stopped!`);
+                window.busyQueue.splice(arrayIndex, 1);
             }
         }
 
         // remove any expired item from the queue
-        window.top.busyQueue.forEach(item => {
+        window.busyQueue.forEach(item => {
             if (item.expiry <= Date.now()) {
-                arrayIndex = window.top.busyQueue.findIndex(busyItem => busyItem.id === item.id);
+                arrayIndex = window.busyQueue.findIndex(busyItem => busyItem.id === item.id);
                 if (arrayIndex > -1) {
-                    if (debug) console.debug(`removing expired busy item "${window.top.busyQueue[arrayIndex].id}"`);
-                    window.top.busyQueue.splice(arrayIndex, 1);
+                    if (debug) console.debug(`removing expired busy item "${window.busyQueue[arrayIndex].id}"`);
+                    window.busyQueue.splice(arrayIndex, 1);
                 }
             }
         })
@@ -78,7 +78,7 @@ export function stop(id) {
 
 // clear the busy queue and stop the busy animation
 export function reset() {
-    window.top.busyQueue = [];
+    window.busyQueue = [];
     _updateBusy();
 }
 
@@ -87,7 +87,7 @@ export function reset() {
 // debug the busy animation queue
 export function status() {
     _initQueue();
-    console.log('busy queue:', window.top.busyQueue);
+    console.log('busy queue:', window.busyQueue);
 }
 
 
@@ -101,20 +101,20 @@ function _elementExists(el) {
 
 
 function _initQueue() {
-    if (typeof window.top.busyQueue !== 'object')
-        window.top.busyQueue = [];
+    if (typeof window.busyQueue !== 'object')
+        window.busyQueue = [];
 }
 
 
 // monitor the queue and update the busy animation
 function _updateBusy() {
-    if (debug) console.debug(`_updateBusy() queue length: ${window.top.busyQueue.length}`);
-    if (window.top.busyQueue.length === 0) {
+    if (debug) console.debug(`_updateBusy() queue length: ${window.busyQueue.length}`);
+    if (window.busyQueue.length === 0) {
         _fadeOutAnimation();
         _fadeOutModal();
     } else {
-        _showAnimation(window.top.busyQueue[0].id);
-        if (window.top.busyQueue.some(item => item.modal))
+        _showAnimation(window.busyQueue[0].id);
+        if (window.busyQueue.some(item => item.modal))
             _showModal()
         else
             _fadeOutModal();
@@ -123,25 +123,25 @@ function _updateBusy() {
 
 
 function _showAnimation(id) {
-    window.top.clearTimeout(fadeoutAnimationTimer);
+    window.clearTimeout(fadeoutAnimationTimer);
 
     // create the busy animation and modal underlay if it doesn't exist
     if (!_elementExists(modalUnderlay)) {
-        modalUnderlay = window.top.document.body.querySelector('#animated-loader-modal-underlay');
+        modalUnderlay = window.document.body.querySelector('#animated-loader-modal-underlay');
         if (modalUnderlay === null) {
-            modalUnderlay = window.top.document.createElement('div');
+            modalUnderlay = window.document.createElement('div');
             modalUnderlay.id = 'animated-loader-modal-underlay';
-            window.top.document.body.appendChild(modalUnderlay);
+            window.document.body.appendChild(modalUnderlay);
         }
     }
 
     if (!_elementExists(busyAnimation)) {
-        busyAnimation = window.top.document.body.querySelector('#animated-loader');
+        busyAnimation = window.document.body.querySelector('#animated-loader');
         if (busyAnimation === null) {
-            busyAnimation = window.top.document.createElement('div');
+            busyAnimation = window.document.createElement('div');
             busyAnimation.id = 'animated-loader';
             busyAnimation.innerHTML = '<div><div><div><div><div><div><div><div><div><div></div></div></div></div></div></div></div></div></div>';
-            window.top.document.body.appendChild(busyAnimation);
+            window.document.body.appendChild(busyAnimation);
         }
     }
 
@@ -154,7 +154,7 @@ function _showAnimation(id) {
 }
 
 function _showModal() {
-    window.top.clearTimeout(fadeoutModalTimer);
+    window.clearTimeout(fadeoutModalTimer);
     if (_elementExists(modalUnderlay)) {
         modalUnderlay.style.display = 'block';
         modalUnderlay.style.opacity = '1';
@@ -162,28 +162,28 @@ function _showModal() {
 }
 
 function _fadeOutAnimation() {
-    window.top.clearTimeout(fadeoutAnimationTimer);
+    window.clearTimeout(fadeoutAnimationTimer);
 
     // fade out
     if (_elementExists(busyAnimation))
         busyAnimation.style.opacity = '0';
 
     // then hide
-    fadeoutAnimationTimer = window.top.setTimeout(() => {
+    fadeoutAnimationTimer = window.setTimeout(() => {
         if (_elementExists(busyAnimation))
             busyAnimation.style.display = 'none';
     }, 500);   // hide
 }
 
 function _fadeOutModal() {
-    window.top.clearTimeout(fadeoutModalTimer);
+    window.clearTimeout(fadeoutModalTimer);
 
     // fadeout
     if (_elementExists(modalUnderlay))
         modalUnderlay.style.opacity = '0';
 
     // then hide
-    fadeoutModalTimer = window.top.setTimeout(() => {
+    fadeoutModalTimer = window.setTimeout(() => {
         if (_elementExists(modalUnderlay))
             modalUnderlay.style.display = 'none';
     }, 500);   // hide
@@ -198,10 +198,10 @@ function _fadeOutModal() {
  */
 function _onTopZIndex() {
     let zTop = 0;
-    const elements = window.top.document.getElementsByTagName('*');
+    const elements = window.document.getElementsByTagName('*');
 
     for (let i = 0; i < elements.length; i++) {
-        let zIndex = window.top.getComputedStyle(elements[i]).getPropertyValue('z-index');
+        let zIndex = window.getComputedStyle(elements[i]).getPropertyValue('z-index');
         zIndex = isNaN(zIndex) ? 0 : parseInt(zIndex);
         if (zIndex && zIndex > zTop)
             zTop = zIndex;
